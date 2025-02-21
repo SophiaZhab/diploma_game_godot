537846a11497ab8022da8d6669b9b093a8f3b64f
SophiaZhab <sophiasonia777@gmail.com>
2025-02-21 16:06:59 +0200

    Перший коміт: додано Godot проєкт

---
+++ Main.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+func _ready():
+	$StartButton.connect("pressed", _on_start_pressed)
+	$ExitButton.connect("pressed", _on_exit_pressed)
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://scenes/Center_street.tscn")
+
+func _on_exit_pressed():
+	get_tree().quit()
